5fc2bcffd00d87fc10bbddd30a26c11e0815c459
vitoriagoveia730-ops <vitoriagoveia730@gmail.com>
2026-04-14 01:43:22 -0300

    Clean up server entries in servers.js
    Removed unused server entries from the servers list.

--- data/servers.js
+++ data/servers.js
@@ -3,14 +3,6 @@ const servers = [
     {
         nome: "tropical rp",
         ip: "149.56.41.55:7770"
-    },
-    {
-        nome: "Servidor RP 2",
-        ip: "192.168.1.1:7777"
-    },
-    {
-        nome: "Servidor Teste",
-        ip: "samp.example.com:7777"
     }
 ];
 
